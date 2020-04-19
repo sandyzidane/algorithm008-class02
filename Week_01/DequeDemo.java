796e33af4ffb797252a1c1f86e6d9c008e31912e
sandy <sandyhysbz@gmail.com>
2020-04-19 22:17:51 +0800

    - 增加Deque使用Demo

---
+++ Week_01/DequeDemo.java
@@ -0,0 +1,23 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DequeDemo {
+    
+    public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<>();
+
+        deque.addFirst("a");
+        deque.addFirst("b");
+        deque.addFirst("c");
+        System.out.println(deque);
+
+        String str = deque.peek();
+        System.out.println(str);
+        System.out.println(deque);
+
+        while (deque.size() > 0) {
+            System.out.println(deque.pop());
+        }
+        System.out.println(deque);
+    }
+}
