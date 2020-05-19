035dd8aba3b2e9af192e49baddea0577c854dbd2
sandy <sandyhysbz@gmail.com>
2020-05-20 00:25:10 +0800

    2020.05.19 刷题

---
+++ Week_06/LemondeChange.java
@@ -0,0 +1,19 @@
+/**
+ * 860. 柠檬水找零
+ */
+public class LemondeChange {
+    
+    class Solution {
+        public boolean lemonadeChange(int[] bills) {
+            int five = 0, ten = 0;
+            for (int b : bills) {
+                if (b == 5) five++;
+                else if (b == 10) {five--; ten++;}
+                else if (ten > 0) {ten--; five--;}
+                else five -= 3;
+                if (five < 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Week_06/ValidPalindromeII.java
@@ -0,0 +1,26 @@
+/**
+ * 680. 验证回文字符串II
+ */
+public class ValidPalindromeII {
+
+    class Solution {
+        public boolean validPalindrome(String s) {
+            int l = -1, r = s.length();
+            while (++l < --r) {
+                if (s.charAt(l) != s.charAt(r)) {
+                    return isPalindromic(s, l - 1, r)
+                        || isPalindromic(s, l, r + 1);
+                }
+            }
+            return true;
+        }
+
+        public boolean isPalindromic(String s, int l, int r) {
+            while (++l < --r) {
+                if (s.charAt(l) != s.charAt(r)) return false;
+            }
+            return true;
+        }
+    }
+    
+}
